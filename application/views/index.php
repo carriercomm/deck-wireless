daa2d18f1d2bba7d6e808ea145ae256dc1dea5b2
Jonathan Irvin <djfoxyslpr@gmail.com>
2013-12-03 17:49:15 -0600

    updated references
    updated references
    
    updated references
    
    updated references

--- application/views/index.php
+++ application/views/index.php
@@ -6,11 +6,11 @@
 <title></title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
-<script type="text/javascript" src="../assets/js/jquery-1.7.1.min.js"></script>
-<script type="text/javascript" src="../assets/js/jquery.slidertron-1.3.js"></script>
+<script type="text/javascript" src="assets/js/jquery-1.7.1.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.slidertron-1.3.js"></script>
 <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
-<link href="../assets/css/default.css" rel="stylesheet" type="text/css" media="all" />
-<link href="../assets/css/fonts.css" rel="stylesheet" type="text/css" media="all" />
+<link href="assets/css/default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="assets/css/fonts.css" rel="stylesheet" type="text/css" media="all" />
 
 <!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
 

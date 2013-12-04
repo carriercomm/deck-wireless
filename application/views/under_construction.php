cf7393050a8dd7977b775e69cab857bb3b9badf8
Jonathan Irvin <djfoxyslpr@gmail.com>
2013-12-04 16:42:28 -0600

    fixing merge, adjusting references

--- application/views/under_construction.php
+++ application/views/under_construction.php
@@ -69,7 +69,11 @@
       </div>
 
       <div class="footer">
+<<<<<<< HEAD
         <p>&copy; <? echo $title; ?> 2013</p>
+=======
+        <p>&copy; Company 2013</p>
+>>>>>>> renamed under_construction file, was mispelled
       </div>
 
     </div> <!-- /container -->

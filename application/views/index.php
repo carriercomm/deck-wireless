0a7b2cfa98f649a19ad2c4709d16849e27be2559
Jonathan Irvin <djfoxyslpr@gmail.com>
2013-12-18 16:49:31 -0600

    added pricing and clickable button on phone under contact info

--- application/views/index.php
+++ application/views/index.php
@@ -10,6 +10,7 @@
 <script type="text/javascript" src="assets/js/jquery.slidertron-1.3.js"></script>
 <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
 <link href="assets/css/default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="assets/css/pricing.css" rel="stylesheet" type="text/css" media="all" />
 <link href="assets/css/fonts.css" rel="stylesheet" type="text/css" media="all" />
 
 <!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
@@ -42,15 +43,110 @@
 			<div class="ebox1">
 				<div class="hexagon"><span class="icon icon-signal"></span></div>
 				<div class="title">
-					<h2>Rate Plans</h2>
+					<h2>Nationwide Plans</h2>
 					<span class="byline">We make it easy to get service</span>
 				</div>
-				<p><!-- TODO check out http://webdesignergeeks.com/tutorials/html-css-tutorials/free-css3-pricing-table/ --></p>
+				<div align="center">
+				<table border="0" align="center">	
+					<tr>
+						<th  align="center">PLANS</th>
+						<th  align="center">PRIMARY<br>MONTHLY<br>CHARGE</th>						
+						<th  align="center">ADD<br>A<br>PARTNER</th>						
+					</tr>
+					<tr>
+						<td align="center">UNLIMITED VOICE &amp; TEXT</td>
+						<td align="center">$69.95</td>						
+						<td align="center">$29.95</td>
+					</tr>
+					<tr>
+						<td align="center">PLAN 500</td>
+						<td align="center">$36.95</td>						
+						<td align="center">$14.95</td>
+					</tr>
+					<tr>
+						<td align="center">PLAN 1000</td>
+						<td align="center">$48.95</td>						
+						<td align="center">$9.95</td>
+					</tr>
+					<tr>
+						<td align="center">PLAN 1500</td>
+						<td align="center">$66.95</td>
+						<td align="center">$9.95</td>
+					</tr>
+					<tr>
+						<td align="center">PLAN 2000</td>
+						<td align="center">$94.95</td>
+						<td align="center">$9.95</td>
+					</tr>
+					<tr>
+						<td align="center">PLAN 2500</td>
+						<td align="center">$109.95</td>
+						<td align="center">$9.95</td>
+					</tr>
+					<tr>
+						<td align="center">PLAN 3500</td>
+						<td align="center">$149.95</td>
+						<td align="center">$9.95</td>
+					</tr>
+					<tr>
+						<td align="center">PLAN 5500</td>
+						<td align="center">$189.95</td>
+						<td align="center">$9.95</td>
+					</tr>					
+					</table>
+					
+					
+					<br><br>
+					<h2>Smartphone &amp;<br>Data Device<br>Plans</h2>
+					
+					<table border=0>
+					<tr>
+						<th align="center">USAGE</th>
+						<th align="center">CHARGE</th>
+						<th align="center">ADD'L USAGE</th>
+					</tr>
+					<tr>
+						<td align="center">2 GB</td>
+						<td align="center">$29.95</td>
+						<td align="center">$10.00/GB</td>
+					</tr>
+					<tr>
+						<td align="center">5 GB</td>
+						<td align="center">$59.95</td>
+						<td align="center"></td>
+					</tr>
+					<tr>
+						<td align="center">10 GB</td>
+						<td align="center">$99.95</td>
+						<td align="center"></td>
+					</tr>
+					<tr>
+						<td align="center">25 GB</td>
+						<td align="center">$159.95</td>
+						<td align="center"></td>
+						<td align="center"></td>
+						<td align="center"></td>
+						<td align="center"></td>
+						<td align="center"></td>
+						<td align="center"></td>
+					</tr>
+					<tr>
+						<td align="center">50 GB</td>
+						<td align="center">$279.95</td>
+						<td align="center"></td>
+					</tr>
+					<tr>
+						<td align="center">100 GB</td>
+						<td align="center">$499.95</td>
+						<td align="center"></td>
+					</tr>
+				</table>
+				</div>
 				<a href="#" class="button icon icon-arrow-up"></a>
 			</div>		
 
 			<div class="ebox2">
-				<div class="hexagon"><span class="icon icon-phone"></span></div>
+				<div class="hexagon"><a href="tel:580-330-3325"><span class="icon icon-phone" style="color:#FFF;text-decoration:none"></span></a></div>
 				<div class="title">
 					<h2>Contact Us</h2>
 					<span class="byline">We would love to hear from you</span>
